32129cba482fe81a5ae3a2d8c48e17d0a3db503a
Ricardo Guerra <RikkuG491@gmail.com>
2024-05-25 21:09:20 -0400

    Updated 1st test and added 2 more tests for burgers variable in indexText.js

--- test/indexTest.js
+++ test/indexTest.js
@@ -12,7 +12,17 @@ describe('index.js', function() {
   describe('burgers', function() {
     it('is declared using const', function() {
       expect(burgers);
-      expect(js).to.match(/const burgers/, 'Expected burgers to be declared using const');
+      expect(js).to.match(/const[ \n]+burgers/, 'Expected burgers to be declared using const');
+    });
+
+    it('is set to the value of an array', function() {
+      expect(burgers);
+      expect(js).to.match(/const[ \n]+burgers[ \n]*=[ \n]*\[(.|\n)*\]/, 'Expected the value of burgers to be an array');
+    });
+
+    it("has the following 2 elements: 'Hamburger' and 'Cheeseburger'", function() {
+      expect(burgers.includes('Hamburger')).to.equal(true, "Expected the burgers array to have the string 'Hamburger' as one of its elements")
+      expect(burgers.includes('Cheeseburger')).to.equal(true, "Expected the burgers array to have the string 'Cheeseburger' as one of its elements")
     });
   });
 });
