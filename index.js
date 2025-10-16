3832eb0a710356bab774b9cbd76761ed6046f1ba
Yonatan Peters <yonipeters7@gmail.com>
2025-10-16 00:30:29 -0400

    Flatburger Restaurant

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+// Global variables
+let burgers = ['Hamburger', 'Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake';
+
+// Function scope
+function addBurger() {
+  let newBurger = 'Flatburger'; // function-scoped variable
+  burgers.push(newBurger);
+}
+
+// Block scope
+if (true) {
+  let anotherNewBurger = 'Maple Bacon Burger'; // block-scoped variable
+  burgers.push(anotherNewBurger);
+
+  // Nested function to update global variable
+  function changeFeaturedDrink() {
+    featuredDrink = 'The JavaShake';
+  }
+
+  changeFeaturedDrink(); // update the drink
+}
+
+// Optional logs to check functionality
+console.log('Menu Burgers:', burgers);
+console.log('Featured Drink:', featuredDrink);
+
+// Export for grading (optional)
+module.exports = { burgers, featuredDrink, addBurger };
